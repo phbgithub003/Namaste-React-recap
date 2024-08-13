9fe033c1e038b9a579977c4b87c86a33981d9a26
harshitp <palacheralaharshit@gmail.com>
2024-08-13 11:13:19 +0530

    Episode-03

--- App.js
+++ App.js
@@ -1,25 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" },
-[
-    React.createElement(
-        "div",
-        {id:"child1"},
-        [
-            React.createElement("h1",{},"This is h1 Tag coming from child1"),
-            React.createElement("h2",{},"This is h2 Tag coming from child1")
-        ]
-    ),
-    React.createElement(
-        "div",
-        {id:"child2"},
-        [
-            React.createElement("h1",{},"This is h1 Tag coming from child2"),
-            React.createElement("h2",{},"This is h2 Tag coming from child2")
-        ]
-    )
-    ]
-);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+// functional component- normal JS function that returns a JSX
+
+const Title = () => (
+	<h1 className="heading">Namaste React From JSX 🚀</h1>
+)
+
+const Heading = () => {
+	return (
+		<div id="container">
+			<Title />
+			<h1 className="heading">Namaste React From Functional Component JSX 🚀</h1>
+		</div>
+	);
+}
+
+// React element using JSX
+const heading = <h1 className="heading">Namaste React From JSX 🚀</h1>
+root.render(<Heading/>);
