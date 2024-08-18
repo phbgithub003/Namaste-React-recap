71ed94e4af8558f780d37c568bf57fb63be22236
harshitp <palacheralaharshit@gmail.com>
2024-08-18 09:21:45 +0530

    Episode-06

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,33 +1,78 @@
-import React, { useState} from "react";
+import React, { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
-import { resList } from "../utils/mockdata";
-
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-	var [hotelList, setHotelList] = useState(resList);
-	return (
-		<div className="body">
-			<div className="filter">
-				<button className="filter-btn" onClick={() => {
-					let data = resList.filter((restaurant) => {
-						return (restaurant.data.avgRating) > 4.0
-					})
-					setHotelList(data);
-				}}
-				>Top Rated Restaurants</button>
-			</div>
-			<div className="res-container">
-				{
-					hotelList.map((restaurant) => {
+  const [hotelList, setHotelList] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredHotelLists, setFilteredHotelLists] = useState([]);
+  async function fetchData() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.406498&lng=78.47724389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setHotelList(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredHotelLists(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  }
 
-						return <RestaurantCard key={restaurant.data.id} resData={restaurant} />;
+  useEffect(() => {
+    fetchData(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.406498&lng=78.47724389999999&restaurantId=4320&catalog_qa=undefined&submitAction=ENTER"
+    );
+  }, []);
 
-					})
-				}
-
-			</div>
-		</div>
-	);
-}
+  return hotelList.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              let filterData = filteredHotelLists.filter((hotel) =>
+                hotel.info.name.toLowerCase().includes(searchText)
+              );
+              setHotelList(filterData);
+            }}
+          >
+            search
+          </button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            // Assuming hotelList is the state containing the list of restaurants
+            let data = hotelList.filter((restaurant) => {
+              return restaurant.info.avgRating > 4.5;
+            });
+            // Update the state with the filtered list
+            setHotelList(data);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {hotelList.map((restaurant) => {
+          return (
+            <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,33 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
-	return (
-		<div className="header">
-			<div className="logo-container">
-				<img className="logoImage" src ={LOGO_URL}></img>
-			</div>
-			<div className="nav-items">
-				<ul>
-					<li>Home</li>
-					<li>About Us</li>
-					<li>Contact Us</li>
-					<li>Cart</li>
-				</ul>
-			</div>
-		</div>
-	);
-}
+  const [loginBtn, setLoginBtn] = useState("Login");
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logoImage" src={LOGO_URL}></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              loginBtn === "Login"
+                ? setLoginBtn("Logout")
+                : setLoginBtn("Login");
+            }}
+          >
+            {loginBtn}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
